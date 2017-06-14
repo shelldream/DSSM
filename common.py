2b818cde1658e31b293fee5ff9b9e2bd0ee23f19
shelldream <shelldream@163.com>
2017-06-14 19:31:12 +0800

    add DataBatchReader

--- common.py
+++ common.py
@@ -6,6 +6,10 @@
 """
 import sys
 reload(sys).setdefaultencoding("utf-8")
+import os
+
+import tensorflow as tf
+import numpy as np
 
 
 def xavier_init(fan_in, fan_out, constant=1):
@@ -23,3 +27,40 @@ def xavier_init(fan_in, fan_out, constant=1):
     return res
 
 
+class DataBatchReader(object):
+    """
+        批量文件读取器
+        读入的文件格式：每行三列
+    """
+    def __init__(self, dirname, field_cnt, record_default="", field_delim="\t", batch_size=5000):
+        if not os.path.exists(dirname):
+            raise ValueError("The data directory %s does not exist!"%dirname)
+        self.dirname = dirname + "/"
+        self.file_list = [self.dirname + filename for filename in os.listdir(self.dirname)]
+        self.filename_queue = tf.train.string_input_producer(self.file_list, shuffle=True)
+        self.batch_size = batch_size
+        self.reader = tf.TextLineReader()
+        key, value = self.reader.read(self.filename_queue)
+        self.content = tf.decode_csv(value, record_defaults=[[record_default] for i in range(field_cnt)], field_delim=field_delim)
+        self.features = tf.stack(self.content, axis=-1)
+
+    def get_batch_data(self):
+        min_after_dequeue = 10000
+        capacity = min_after_dequeue +  self.batch_size
+        batch_f = tf.train.shuffle_batch([self.features], batch_size=self.batch_size, capacity=capacity, min_after_dequeue=min_after_dequeue)
+
+        init = tf.initialize_all_variables()
+        with tf.Session() as sess:
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
+            sess.run(init)
+
+            output = sess.run(batch_f)
+            coord.request_stop()
+            coord.join(threads)
+        return output 
+
+if __name__ == "__main__":
+    test_dir = "/home/admin/huangxiaojun/data/DSSM"
+    batch_reader = DataBatchReader(test_dir, field_cnt=3)
+    output = batch_reader.get_batch_data()
--- dssm.py
+++ dssm.py
@@ -6,14 +6,15 @@
 """
 import sys
 reload(sys).setdefaultencoding("utf-8")
+import os
+
 import numpy as np
 import tensorflow as tf
 from common import xavier_init
 
 class DSSM(object):
-    def __init__(self, n_input, n_l1_hidden, n_l2_hidden, \
-            transfer_function=tf.nn.relu, optimizer=tf.train.AdamOptimizer, is_dropout=True, drop_rate=0.5)
-        self.weights = self._initialize_weights()
+    def __init__(self, n_input, n_l1_hidden, n_l2_hidden, transfer_function=tf.nn.relu, \
+                optimizer=tf.train.AdamOptimizer(), is_dropout=True, drop_rate=0.5, is_sparse=False, batch_size=1000):
         self.n_input = n_input
         self.n_l1_hidden = n_l1_hidden
         self.n_l2_hidden = n_l2_hidden
@@ -21,16 +22,30 @@ class DSSM(object):
         self.optimizer = optimizer
         self.is_dropout = is_dropout
         self.drop_rate = drop_rate
-
-        self.query_batch = tf.sparse_placeholder(tf.float32, [None, self.n_input], name="QueryBatch")
-        self.pos_doc_batch = tf.sparse_placeholder(tf.float32, [None, self.n_input], name="PosDocBatch")
-        self.neg_doc_batch = tf.sparse_placeholder(tf.float32, [None, self.n_input], name="NegDocBatch")
+        self.is_sparse = is_sparse
+        self.batch_size = batch_size
         self.weight = self._initialize_weights()
         
+        if self.is_sparse:
+            self.query_batch = tf.sparse_placeholder(tf.float32, shape=np.array([batch_size, self.n_input], np.int64), name="QueryBatch")
+            self.pos_doc_batch = tf.sparse_placeholder(tf.float32, shape=np.array([batch_size, self.n_input], np.int64), name="PosDocBatch")
+            self.neg_doc_batch = tf.sparse_placeholder(tf.float32, shape=np.array([batch_size, self.n_input], np.int64), name="NegDocBatch")
+            #self.query_batch = tf.sparse_placeholder(tf.float32, None, name="QueryBatch")
+            #self.pos_doc_batch = tf.sparse_placeholder(tf.float32, None, name="PosDocBatch")
+            #self.neg_doc_batch = tf.sparse_placeholder(tf.float32, None, name="NegDocBatch")
+        else:
+            self.query_batch = tf.placeholder(tf.float32, [None, self.n_input], name="QueryBatch")
+            self.pos_doc_batch = tf.placeholder(tf.float32, [None, self.n_input], name="PosDocBatch")
+            self.neg_doc_batch = tf.placeholder(tf.float32, [None, self.n_input], name="NegDocBatch")
+        
+        print self.query_batch.get_shape()
+        print self.pos_doc_batch.get_shape()
+        print self.neg_doc_batch.get_shape()
+
         # loss function
-        self.query_output = self._model_output(self.query_batch)
-        self.pos_doc_output = self._model_output(self.pos_doc_batch)
-        self.neg_doc_output = self._model_output(self.neg_doc_batch)
+        self.query_output = self.get_model_output(self.query_batch)
+        self.pos_doc_output = self.get_model_output(self.pos_doc_batch)
+        self.neg_doc_output = self.get_model_output(self.neg_doc_batch)
         pos_sim = self._cal_cos_sim(self.query_output, self.pos_doc_output)
         neg_sim = self._cal_cos_sim(self.query_output, self.neg_doc_output)
         delta_sim = pos_sim - neg_sim
@@ -38,9 +53,14 @@ class DSSM(object):
         self.optimizer = optimizer.minimize(self.loss)
         
         init = tf.global_variables_initializer()
-        self.sess = tf.session()
+        self.sess = tf.Session()
         self.sess.run(init)
-
+        
+        #模型保存
+        self.saver = tf.train.Saver()
+        if not os.path.exists("./model"):
+            os.popen("mkdir ./model")
+        self.save_path = "./model/model_default.ckpt"
 
     def _cal_cos_sim(self, data1, data2):
         """计算两个维度相同的张量间的余弦距离"""
@@ -51,9 +71,12 @@ class DSSM(object):
         cos_sim = tf.truediv(product, norm_product)
         return cos_sim
 
-    def _model_output(self, data_batch):
+    def get_model_output(self, data_batch):
         """构建模型""" 
-        data_l1 = tf.sparse_tensor_dense_matmul(data_batch, self.weight["w1"]) + self.weight["b1"]
+        if self.is_sparse:
+            data_l1 = tf.sparse_tensor_dense_matmul(data_batch, self.weight["w1"]) + self.weight["b1"]
+        else:
+            data_l1 = tf.matmul(data_batch, self.weight["w1"]) + self.weight["b1"]
         data_l1_output = self.transfer_function(data_l1)
         if self.is_dropout:
             data_l1_output = tf.nn.dropout(data_l1_output, self.drop_rate)
@@ -74,13 +97,25 @@ class DSSM(object):
         all_weight["b2"] = tf.Variable(tf.zeros([self.n_l2_hidden], dtype=tf.float32))
         return all_weight
 
-    def partial_fit(self, X):
+    def partial_fit(self, query_x, pos_doc_x, neg_doc_x):
         """用一个batch数据进行训练并返回当前的损失
             Args:
                 X: 一个batch的训练数据
             Returns:
-                cost: 当前训练的损失
+                loss: 当前训练的损失
         """
-        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict = {self.x: X})
-        return cost
+        loss, opt = self.sess.run((self.loss, self.optimizer), \
+            feed_dict = {self.query_batch:query_x, self.pos_doc_batch:pos_doc_x, self.neg_doc_batch:neg_doc_x})
+        return loss
+    
+    def save_model(self, save_path="./model/model_default.ckpt"):
+        """保存模型至特定路径"""
+        self.save_path = save_path
+        self.saver.save(self.sess, save_path)
+        print "The model has been saved in the file: %s"%self.save_path
 
+    def load_model(self, save_path="./model/model_default.ckpt"):
+        """载入已有模型"""
+        if os.path.exists(save_path):
+            self.saver.restore(save_path)
+            print "The model %s has been loaded!"%save_path
